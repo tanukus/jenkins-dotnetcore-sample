ec154972afe28894a4bbe7592e21dc9cdb693d31
tanukus <57962462+tanukus@users.noreply.github.com>
2020-05-05 18:49:55 -0400

    Initial (#1)
    Co-authored-by: Sudarshan Tanuku <sudarshan.v-ctr.tanuku@faa.gov>

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace jenkins_dotnetcore_sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
